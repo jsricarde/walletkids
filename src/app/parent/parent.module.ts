dd9a7eacc041b308c0361caf51883232397ad26b
Jeffry Lenis <jeffry.lenis@endava.com>
2018-09-03 13:14:40 -0500

    A03 - UI Structure.
    * Add material components.
    * Add header nav bar.

--- src/app/parent/parent.module.ts
+++ src/app/parent/parent.module.ts
@@ -5,9 +5,10 @@ import { ParentRoutingModule } from './parent-routing.module';
 import { ParentComponent } from './parent/parent.component';
 import { SharedModule } from '../shared/shared.module';
 import { StudentComponent } from './student/student.component';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
-  imports: [CommonModule, ParentRoutingModule, SharedModule],
+  imports: [CommonModule, ParentRoutingModule, SharedModule, ComponentsModule],
   declarations: [ParentComponent, StudentComponent]
 })
 export class ParentModule {}
--- src/app/pos/pos/pos.component.ts
+++ src/app/pos/pos/pos.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pos.component.css']
 })
 export class PosComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
