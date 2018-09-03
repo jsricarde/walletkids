adfe9eccc70bba6eefdac63a4b3a5bfd8b23996a
Diego Jimenez <diego.jimenez@endava.com>
2018-09-03 14:17:21 -0500

    Changes from codereview feedback

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QrReaderComponent } from './qr-reader/qr-reader.component';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
-import { MAT_DIALOG_DATA } from '@angular/material';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from '../shared/shared.module';
 
