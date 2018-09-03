0e7442143ccd797c17086488741b39b0cef995fe
Sebastian <jsricarde@gmail.com>
2018-09-03 14:25:25 -0500

    A05 - UI POS
      * Add grid products

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [CommonModule, MatSidenavModule, MatDividerModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule
+  ],
   declarations: [],
-  exports: [MatSidenavModule, MatDividerModule]
+  exports: [
+    MatSidenavModule,
+    MatDividerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule
+  ]
 })
 export class SharedModule {}
